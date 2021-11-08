6e7fb45208199d789feba1fc0e8066ce6868da6b
NasiphiM <93937047+NasiphiM@users.noreply.github.com>
2021-11-09 00:56:51 +0200

    Add files via upload

---
+++ DroneShip.cpp
@@ -0,0 +1,14 @@
+#include "DroneShip.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void DroneShip::hasLanded(){
+	if (landed == "True") {
+		//SpaceX lands the first stage on a drone ship in the middle of the ocean
+		cout<<"Rocket has landed on drone Ship"<<endl; 
+	}
+}
+	
---
+++ DroneShip.h
@@ -0,0 +1,19 @@
+#ifndef DRONESHIP_H
+#define DRONESHIP_H
+
+#include "SpaceCommand.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class DroneShip : public SpaceCommand{
+	private:
+		bool landed;
+	public:
+		
+		void hasLanded();
+	
+};
+#endif 
+
