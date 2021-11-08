3cf73112f3e938677d0cd9160cceb3e301b6123b
NasiphiM <93937047+NasiphiM@users.noreply.github.com>
2021-11-09 01:31:44 +0200

    Update DroneShip.cpp

--- DroneShip.cpp
+++ DroneShip.cpp
@@ -5,8 +5,17 @@
 
 using namespace std;
 
-void DroneShip::hasLanded(){
-	if (landed == "True") {
+bool DroneShip:: HasLanded(){
+	if (landed == true) {
+	    return true;
+	}
+	else{
+		return false;
+	}
+}
+
+void DroneShip::print(){
+	if ( landed == true) {
 		//SpaceX lands the first stage on a drone ship in the middle of the ocean
 		cout<<"Rocket has landed on drone Ship"<<endl; 
 	}
