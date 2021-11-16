77a9c464696b80febc3de5fe4f59ee1ed2690ddc
Yane <93937046+V0iDy9@users.noreply.github.com>
2021-11-17 00:01:13 +0200

    Update Engine.h

--- Engine.h
+++ Engine.h
@@ -3,10 +3,15 @@
 
 class Engine
 {
+    private:
+        string engineType; //String variable to disntinguish between the types of engines (VacuumMerlin/Merlin)
     public:
         Engine();
+        Engine(string etype);
         virtual ~Engine();
         virtual void operation() = 0;
+        string getType();
+        void setType(string et);
 };
 
 #endif
