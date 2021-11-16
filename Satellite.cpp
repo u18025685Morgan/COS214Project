e75f3d4f4b7e56b568922ef4ba07049568ecc17e
Sengi Sono <85702473+SengiSliko@users.noreply.github.com>
2021-11-17 00:21:18 +0200

    Update Satellite.cpp

--- Satellite.cpp
+++ Satellite.cpp
@@ -1,5 +1,19 @@
-//
+//s
 // Created by lisar on 2021/11/16.
 //
 
 #include "Satellite.h"
+
+ void Satellite :: changed()
+{
+
+}
+void Satellite :: get()
+{
+    
+}
+
+void Satellite :: set(SpaceCommand *sc)
+{
+    SC = sc;
+} 
