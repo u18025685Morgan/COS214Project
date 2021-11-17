4d2768b216fbe49ad3c468b37c84c7d88002afe0
Yane <93937046+V0iDy9@users.noreply.github.com>
2021-11-17 15:18:21 +0200

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -4,6 +4,41 @@ using namespace std;
 Engine::Engine()
 {}
 
+Engine::Engine(string etype)
+{
+    engineType = etype;
+}
+
+Engine::Engine(int n)
+{
+    numEngines = n;
+}
+
+Engine::Engine(string etype, int n)
+{
+    engineType = etype;
+    numEngines = n;
+}
+
 Engine::~Engine()
 {}
 
+string Engine::getType()
+{
+    return engineType;
+}
+
+void Engine::setType(string et)
+{
+    engineType = et;
+}
+
+int Engine::getNumEngines()
+{
+    return numEngines;
+}
+
+void Engine::setNumEngines(int nr)
+{
+    numEngines = nr;
+}
