9b986c59c75a67756cab92f2f259e9017cf5bd68
Yane <93937046+V0iDy9@users.noreply.github.com>
2021-11-18 00:34:24 +0200

    Update Falcon9Factory.h

--- Falcon9Factory.h
+++ Falcon9Factory.h
@@ -1,7 +1,6 @@
-#include "RocketFactory.h"
 #ifndef FALCON9FACTORY_H
 #define FALCON9FACTORY_H
-
+#include "RocketFactory.h"
 
 class Falcon9Factory : public RocketFactory //A ConcreteCreator participant.
 {
