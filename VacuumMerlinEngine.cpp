b414b44ae5892baad520fcd2b302e987708c0264
Yane <93937046+V0iDy9@users.noreply.github.com>
2021-11-17 15:24:04 +0200

    Update VacuumMerlinEngine.cpp

--- VacuumMerlinEngine.cpp
+++ VacuumMerlinEngine.cpp
@@ -1,9 +1,13 @@
 #include "VacuumMerlinEngine.h"
+#include <string>
+#include <iostream>
 using namespace std;
 
 VacuumMerlinEngine::VacuumMerlinEngine()
-{}
+{
+    cout<<"Building a Vacuum Merlin Engine"<<endl;
+}
 
-VacuumMerlinEngine::~VacuumMerlinEngine();
+VacuumMerlinEngine::~VacuumMerlinEngine()
 {}
 
