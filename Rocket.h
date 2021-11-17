53aa455731fabc08b17d49fa30e94cccb5874584
Yane <93937046+V0iDy9@users.noreply.github.com>
2021-11-17 16:27:28 +0200

    Update Rocket.h

--- Rocket.h
+++ Rocket.h
@@ -6,20 +6,20 @@
 
 using namespace std;
 
-class Rocket : public SpaceCommand //The Product participant of the Factory Method design pattern.
+class Rocket : public SpaceCommand    //The Product participant of the Factory Method design pattern.
 {
     private:
-        string name; //A way to see with what type of rocket you are working with.
+        string name;                  //A way to see with what type of rocket you are working with.
     public:
-        Rocket();                   //Constructor.
-        Rocket(string rocketname);
-        string getName();           //Returns the name/type of rocket.
-        void setName(string n);     //Sets the name/type of rocket.
+        Rocket();                     //Default constructor.
+        Rocket(string rocketname);    //Constructor taking the name of the rocket as a parameeter.
+        string getName();             //Returns the name/type of rocket.
+        void setName(string n);       //Sets the name/type of rocket.
         void launchRocket();
         void print();
 
-        void On();                  //turns Rocket on ->(Command dp)
-        void Off();                 //turns Rocket off ->(Command dp)
+        void On();                    //turns Rocket on ->(Command dp)
+        void Off();                   //turns Rocket off ->(Command dp)
 
     protected:
         virtual void launch() = 0;
