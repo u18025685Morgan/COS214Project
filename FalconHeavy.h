9f6baf7afd685d3b58c6502ef403601af859c828
Yane <93937046+V0iDy9@users.noreply.github.com>
2021-11-18 16:32:27 +0200

    Update FalconHeavy.h

--- FalconHeavy.h
+++ FalconHeavy.h
@@ -1,11 +1,16 @@
 #ifndef FALCONHEAVY_H
 #define FALCONHEAVY_H
+#include <string>
 #include "Rocket.h"
+using namespace std;
 
 class FalconHeavy : public Rocket //A ConcreteProduct participant of the Factory Method design pattern.
 {
+    public:
+        FalconHeavy(string  = "Falcon Heavy");
     protected:
         void launch();
+        void build();
 };
 
 #endif
