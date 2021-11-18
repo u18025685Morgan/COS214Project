e0b7517027e63a250d296dd7235cf87fd4d97d18
u19123460 <91542695+u19123460@users.noreply.github.com>
2021-11-18 16:18:09 +0200

    Delete StageState directory

--- StageState/StageOne/SeperationState.cpp
+++
@@ -1,9 +0,0 @@
-#include "SeperationState.h"
-#include <iostream>
-
-SeperationState::SeperationState(){
-	this->stageOneState = "seperating";
-}
-void SeperationState::handle(){
-	cout << "Stage one is seperating and ready to recovery" << endl;
-}
