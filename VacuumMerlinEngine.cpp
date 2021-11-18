d15205772505f853673d3d862718b21523c00f0e
NasiphiM <93937047+NasiphiM@users.noreply.github.com>
2021-11-18 17:12:38 +0200

    Update VacuumMerlinEngine.cpp

--- VacuumMerlinEngine.cpp
+++ VacuumMerlinEngine.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 using namespace std;
 
+//added by Nasiphi  -17h09
+VacuumMerlinEngine::VacuumMerlinEngine(){
+}
+
 VacuumMerlinEngine::VacuumMerlinEngine(Engine* e):EngineDecorator(e)
 {
 
