1607a7ba2a9664d8bdf6fab817fe24116382c21a
Yane <93937046+V0iDy9@users.noreply.github.com>
2021-11-18 16:23:05 +0200

    Update Rocket.cpp

--- Rocket.cpp
+++ Rocket.cpp
@@ -1,7 +1,10 @@
 #include "Rocket.h"
 #include "RocketState.h"
 #include "RocketInitialState.h"
-
+#include "Engine.h"             //Added by Yane at 16:21
+#include "EngineDecorator.h"
+#include "MerlinEngine.h"
+#include "VacuumMerlinEngine.h"
 #include <iostream>
 using namespace std;
 
@@ -49,5 +52,35 @@ bool Rocket ::staticFireTest(RocketInitialState* obj){
     }
     else{
         return false; 
+    }
+}
+
+void Rocket::buildRocket()  //Added by Yane at 16:22
+{
+    build();
+    Engine* core = new Core();
+    if (name == "Falcon 9")
+    {
+        e = new Engine*[10];
+
+        for (int i=0; i<9; i++)
+        {
+            e[i] = new MerlinEngine(core);
+        }
+        e[9] = new VacuumMerlinEngine(core);
+        cout<<"9 Merlin Engines and 1 Vacuum Merlin Engine has been added"<<endl;
+
+    }
+    else
+    {
+        e = new Engine*[28];
+
+        for (int i=0; i<27; i++)
+        {
+            e[i] = new MerlinEngine(core);
+        }
+        e[27] = new VacuumMerlinEngine(core);
+        cout<<"27 Merlin Engines and 1 Vacuum Merlin Engine has been added"<<endl;
+
     }
 }
