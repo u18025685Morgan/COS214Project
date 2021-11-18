c154716bf3da0195b78933a99f294a0c9fc34b55
Yane <93937046+V0iDy9@users.noreply.github.com>
2021-11-18 16:30:10 +0200

    Update Falcon9.h

--- Falcon9.h
+++ Falcon9.h
@@ -4,8 +4,11 @@
 
 class Falcon9 : public Rocket //A ConcreteProduct participant of the Factory Method design pattern.
 {
+    public:
+        Falcon9(string = "Falcon 9"); //Added by Yane at 16:30
     protected:
         void launch();
+        void build(); //Added by Yane at 16:28
 };
 
 #endif
