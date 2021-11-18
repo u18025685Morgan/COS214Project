430cb81f34d28fa48b24ae33c39d43853eb4fbd3
Yane <93937046+V0iDy9@users.noreply.github.com>
2021-11-18 16:33:11 +0200

    Update FalconHeavy.cpp

--- FalconHeavy.cpp
+++ FalconHeavy.cpp
@@ -3,6 +3,16 @@
 #include <string>
 using namespace std;
 
+FalconHeavy::FalconHeavy(string nameR):Rocket(nameR)
+{
+
+}
+void FalconHeavy::build()
+{
+    cout<<"A Falcon Heavy rocket is being build."<<endl;
+
+}
+
 void FalconHeavy::launch()
 {
     cout<<"A Falcon Heavy rocket launches."<<endl;
