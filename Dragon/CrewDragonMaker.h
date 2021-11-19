8375f40899942e49cede94c0f2206accdf77f73c
thetawanda <93973426+thetawanda@users.noreply.github.com>
2021-11-20 00:34:08 +0200

    Add files via upload

---
+++ Dragon/CrewDragonMaker.h
@@ -0,0 +1,43 @@
+#ifndef CrewDragonMaker_H
+#define CrewDragonMaker_H 
+#include <iostream>
+#include <vector>
+#include "Dragon.h"
+using namespace std;
+
+struct CrewDetails
+{
+	string name;
+	string sex;
+	int age;
+	int weight;
+};
+
+class CrewDragonMaker : public Dragon
+{
+public:
+
+	CrewDragonMaker();
+	~CrewDragonMaker();
+	CrewDragonMaker(string);
+	void setName(string);
+	string getName();
+	void selectNumberSeats(int x);
+	void insertCrewMember(string,string,int,int);
+	void removeCrewMember(int);
+	void getCrewInfo();
+	int getNumSeatsOccupied();
+	int getNumberSeats();
+	void setPayloadMass(int);
+	int getPayloadMass();
+	void stageseparation();
+	void returntoearth();
+
+private:
+	string name;
+	int numseats;
+	int payloadmass;
+	std::vector<CrewDetails> Crew;
+};
+
+#endif 
