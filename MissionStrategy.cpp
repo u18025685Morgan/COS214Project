fe5cd1ee97bdb5feee5ce4869ca61b8d03d86d63
lisaJFellingham <93034244+lisaJFellingham@users.noreply.github.com>
2021-11-21 17:32:47 +0200

    Add files via upload

--- MissionStrategy.cpp
+++ MissionStrategy.cpp
@@ -1,4 +1,8 @@
-#include "missionStrategy.h"
+#include "MissionStrategy.h"
+#include "Falcon9.h"
+#include "FalconHeavy.h"
+#include "Falcon9Factory.h"
+#include "FalconHeavyFactory.h"
 
 MissionStrategy::MissionStrategy(string name)
 {
@@ -8,7 +12,7 @@ MissionStrategy::MissionStrategy(string name)
 
 MissionStrategy::~MissionStrategy()
 {
-    
+
 
 }
 
@@ -19,7 +23,7 @@ void MissionStrategy::setName(string n)
 
 RocketStrategy::RocketStrategy() : MissionStrategy("rocket")
 {
-    
+
 }
 
 
@@ -28,46 +32,46 @@ void RocketStrategy::buildMission()
     int rocket;
     cout << "What kind of Rocket would you like to use for this mission strategy? 1 - Falcon9, 2 - FalconHeavy" << endl;
     cin >> rocket;
-    
-    
-    
+
+
+
     if(rocket == 1)
     {
         RocketFactory * n;
         n = new Falcon9Factory();
-        Rocket nine;
-        nine->build();
-        nine->buildRocket();
+        Falcon9 nine;
+        n->build();
+        nine.buildRocket();
     }
     else if (rocket == 2)
     {
         RocketFactory * h;
         h = new FalconHeavyFactory();
-        Rocket heavy;
-        heavy->build();
-        heavy->buildRocket();
+        FalconHeavy heavy;
+        h->build();
+        heavy.buildRocket();
     }
     else
     {
         cout << "You did not chose a valid answer, program will exit" << endl;
     }
-    
+
 }
 
 EngineStrategy::EngineStrategy() : MissionStrategy("engine")
 {
-    
+
 }
 
 
 void EngineStrategy::buildMission()
 {
-    
+
 }
 
 DragonStrategy::DragonStrategy() : MissionStrategy("engine")
 {
-    
+
 }
 
 
