b1913d12d29e6f0e3474ad8fbd5cd608a0170c6c
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2021-11-23 00:13:51 +0200

    Update Rocket.h

--- System/Rocket.h
+++ System/Rocket.h
@@ -29,7 +29,7 @@ class Rocket : public SpaceCommand
             string getName();             //Returns the name/type of rocket.
             void setName(string n);       //Sets the name/type of rocket.
             void launchRocket();
-            void buildRocket();           //Added by Yane at 16:18
+            void buildRocket();           //Builds the rocket and adds the engines required to each rocket.
             virtual void print();
             void setNumEngines(int num);    //added by Nasiphi
             void On();                    //turns Rocket on ->(Command dp)
