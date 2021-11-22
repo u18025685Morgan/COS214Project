2ad7a725f2c30afee92fa31ae9454ad26cfa8eef
Morgan Else <88438748+u18025685Morgan@users.noreply.github.com>
2021-11-22 13:46:33 +0200

    Add Comments

--- Mission.cpp
+++ Mission.cpp
@@ -2,19 +2,20 @@
 
 Mission::Mission(MissionStrategy * strategy)
 {
-    this->strategy = strategy;
+    this->strategy = strategy;                      //! copy constructor taking a MissionStrategy object 
 }
 
 
 Mission::~Mission()
 {
-    cout << "Mission Course Completed" << endl;
+    cout << "Mission Course Completed" << endl;        //! Destructor outputs that the mission is over and the strategy is deleted
     delete strategy;
     strategy = 0;
 }
 
 void Mission::startMission()
 {
-    return strategy->buildMission();
+    return strategy->buildMission();                //! returns the strategy's buildMission() function which is implemented in the
+                                                    //! ConcreteStrategy class
 }
 
