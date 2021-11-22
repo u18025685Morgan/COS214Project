43f8526b2df1743638f028869d6d8c54dc8fafef
Morgan Else <88438748+u18025685Morgan@users.noreply.github.com>
2021-11-22 14:23:33 +0200

    Add Comments

--- MissionStrategy.cpp
+++ MissionStrategy.cpp
@@ -4,26 +4,33 @@
 #include "Falcon9Factory.h"
 #include "FalconHeavyFactory.h"
 
-MissionStrategy::MissionStrategy(string name)
+MissionStrategy::MissionStrategy(string name)               //! Constructor that takes the name of the mission strategy as a parameter
 {
-    missionStratName = name;
+    missionStratName = name;                                //! Also outputs a string that has the name 
     cout << "Mission strategy: " << missionStratName << " chosen." << endl;
 }
 
 MissionStrategy::~MissionStrategy()
 {
-
-
 }
 
 void MissionStrategy::setName(string n)
 {
-    missionStratName = n;
+    missionStratName = n;                   //! setter of missionStratName
 }
+
+string MissionStrategy::getName()
+{
+    return missionStratName;                //! getter of missionStratName
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// RocketStrategy functions
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 RocketStrategy::RocketStrategy() : MissionStrategy("rocket")
 {
-
+                            //! uses base class constructor to show that a rocket is being chosen
 }
 
 
@@ -33,34 +40,38 @@ void RocketStrategy::buildMission()
     cout << "What kind of Rocket would you like to use for this mission strategy? 1 - Falcon9, 2 - FalconHeavy" << endl;
     cin >> rocket;
 
-
+    //! Asks client what they want and then depending on that a rocket is created using the Factory method
 
     if(rocket == 1)
     {
         RocketFactory * n;
         n = new Falcon9Factory();
         Falcon9 nine;
-        n->build();
-        nine.buildRocket();
+        n->build();                 //! outputs that a Falcon9 is being built
+        nine.buildRocket();         //! actually builds the rocket
     }
     else if (rocket == 2)
     {
         RocketFactory * h;
         h = new FalconHeavyFactory();
         FalconHeavy heavy;
-        h->build();
-        heavy.buildRocket();
+        h->build();                 //! outputs that a FalconHeavy is being built
+        heavy.buildRocket();        //! actually builds the rocket
     }
     else
     {
-        cout << "You did not chose a valid answer, program will exit" << endl;
+        cout << "You did not chose a valid answer, program will exit" << endl;          //! Shows the client that the input was invalid
     }
 
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// EngineStrategy functions
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 EngineStrategy::EngineStrategy() : MissionStrategy("engine")
 {
-
+                                //! uses base class constructor to show that a rocket is being chosen
 }
 
 
@@ -69,9 +80,13 @@ void EngineStrategy::buildMission()
 
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/// DragonStrategy Functions
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 DragonStrategy::DragonStrategy() : MissionStrategy("engine")
 {
-
+                                //! uses base class constructor to show that a rocket is being chosen
 }
 
 
