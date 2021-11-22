2ef72576c01b83d932fbf91c29af0d3eed0ef02c
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2021-11-22 14:53:03 +0200

    Add files via upload

---
+++ SiphiHelp/Main.cpp
@@ -0,0 +1,28 @@
+#include "Rocket.h"
+#include "RocketFactory.h"
+#include "Falcon9Factory.h"
+#include "FalconHeavyFactory.h"
+#include "Falcon9.h"
+#include "FalconHeavy.h"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout<<"Testing code and compilation."<<endl;
+    
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
+
