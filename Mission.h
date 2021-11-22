5e5bdd1512e964c215aa18db658df3b1e33c4e34
Morgan Else <88438748+u18025685Morgan@users.noreply.github.com>
2021-11-22 13:42:44 +0200

    Add Comments

--- Mission.h
+++ Mission.h
@@ -2,19 +2,18 @@
 #define MISSION_H
 #include <iostream>
 using namespace std;
-#include "Engine.h"
-
-#include "missionStrategy.h"
+#include "Engine.h"             //! Engine Hierarchy uses the Mission class 
+#include "missionStrategy.h"    //! added the Strategy class component of the Strategy design pattern
 
 class Mission
 {
 private:
-    MissionStrategy * strategy;
+    MissionStrategy * strategy; //! pointer to strategy object, varies depending on the concrete strategy chosen
     
 public:
-    Mission(MissionStrategy * strategy);
-    ~Mission();
-    void startMission();
+    Mission(MissionStrategy * strategy);    //! copy constructor
+    ~Mission();                             //! destructor
+    void startMission();                    //! main function that commences the mission with the selected strategies
 
 };
 
