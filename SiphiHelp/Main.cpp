dd63a7d57f802517a75680a597d9459d0ee48ca3
Yane van der Westhuizen <93937046+yanevdw19021209@users.noreply.github.com>
2021-11-22 18:36:35 +0200

    Update Main.cpp

--- SiphiHelp/Main.cpp
+++ SiphiHelp/Main.cpp
@@ -1,9 +1,42 @@
+#include "Engine.h"
+#include "EngineDecorator.h"
+#include "MerlinEngine.h"
+#include "VacuumMerlinEngine.h"
+#include "Command.h"
+#include "TurnOn.h"
+#include "TurnOff.h"
+#include "SpaceCommand.h"
+#include "DroneShip.h"
+#include "Button.h"
+#include "Starlink.h"
+#include "RocketState.h"
+#include "RocketNoState.h"
+#include "RocketStageTwoState.h"
+#include "RocketStageOneState.h"
+#include "RocketLaunchState.h"
+#include "RocketStaticFireState.h"
+#include "RocketInitialState.h"
+#include "Satellite.h"
+#include "GroundReceiver.h"
 #include "Rocket.h"
 #include "RocketFactory.h"
 #include "Falcon9Factory.h"
 #include "FalconHeavyFactory.h"
 #include "Falcon9.h"
 #include "FalconHeavy.h"
+#include "StageOneState.h"
+#include "StageOneSeperationState.h"
+#include "StageOneLaunchState.h"
+#include "StageOneTestState.h"
+#include "StageOneReadyState.h"
+#include "StageTwoState.h"
+#include "StageTwoSeperationState.h"
+#include "StageTwoLaunchState.h"
+#include "StageTwoReadyState.h"
+#include "CrewDragonMaker.h"
+#include "CargoDragonMaker.h"
+#include "MissionStrategy.h"
+#include "Mission.h"
 
 #include <iostream>
 #include <string>
@@ -12,7 +45,7 @@ using namespace std;
 int main()
 {
     cout<<"Testing code and compilation."<<endl;
-    
+
 
 
 
