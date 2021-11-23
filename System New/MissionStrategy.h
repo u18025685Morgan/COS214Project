39a5d5133073289fda60c71c55dc11729fc35f8b
Morgan Else <88438748+u18025685Morgan@users.noreply.github.com>
2021-11-23 08:46:48 +0200

    Update MissionStrategy.h

--- System New/MissionStrategy.h
+++ System New/MissionStrategy.h
@@ -15,17 +15,17 @@ class MissionStrategy           //! Strategy class of the Strategy design patter
             string rocketType;
 
         public:
-            MissionStrategy(string name);   //! Constructor that takes the name of the mission as a parameter
+            MissionStrategy(string name);   //! @brief Constructor that takes the name of the mission as a parameter
                                             //! @param name must be a valid string
-            ~MissionStrategy();             //! Destructor
-            virtual void buildMission();    //! virtual function that builds specific parts of the mission depending on what the client wants
-            void setName(string n);         //! setter function to set the name of the mission strategy
+            ~MissionStrategy();             //! @brief Destructor
+            virtual void buildMission();    //! @brief virtual function that builds specific parts of the mission depending on what the client wants
+            void setName(string n);         //! @brief setter function to set the name of the mission strategy
                                             //! @param name must be a valid string
-            string getName();               //! getter function to get the name of the mission strategy
+            string getName();               //! @brief getter function to get the name of the mission strategy
                                             //! @return string The name of the mission strategy
-            void setRocketChoice(Rocket * r); //! setter function to set the rocketChoice
+            void setRocketChoice(Rocket * r); //! @brief setter function to set the rocketChoice
                                               //! @param r must be an instantiated Rocket
-            Rocket* getRocketChoice();       //! getter function to get the rocketChoice
+            Rocket* getRocketChoice();       //! @brief getter function to get the rocketChoice
                                                //! @return Rocket * The rocket chosen 
 
         };
@@ -33,24 +33,24 @@ class MissionStrategy           //! Strategy class of the Strategy design patter
 class RocketStrategy : public MissionStrategy   //! ConcreteStrategy class in the Strategy design pattern
         {
         public:
-            RocketStrategy();               //! Constructor that uses the base class's constructor
+            RocketStrategy();               //! @brief Constructor that uses the base class's constructor
 
-            virtual void buildMission();    //! Implementation of the buildMission function that asks the client which type of rocket to use
+            virtual void buildMission();    //! @brief Implementation of the buildMission function that asks the client which type of rocket to use
 
         };
 
 class EngineStrategy : public MissionStrategy   //! ConcreteStrategy class in the Strategy design pattern
         {
         public:
-            EngineStrategy();               //! Constructor that uses the base class's constructor
+            EngineStrategy();               //! @brief Constructor that uses the base class's constructor
 
-            virtual void buildMission();     //! Implementation of the buildMission function that asks the client which type of engine to use
+            virtual void buildMission();     //! @brief Implementation of the buildMission function that asks the client which type of engine to use
         };
 
 class DragonStrategy : public MissionStrategy    //! ConcreteStrategy class in the Strategy design pattern
         {
         public:
-            DragonStrategy();               //! Constructor that uses the base class's constructor
-            virtual void buildMission();     //! Implementation of the buildMission function that asks the client which type of dragon to use
+            DragonStrategy();               //! @brief Constructor that uses the base class's constructor
+            virtual void buildMission();     //! @brief Implementation of the buildMission function that asks the client which type of dragon to use
         };
 #endif // MISSIONSTRATEGY_H
