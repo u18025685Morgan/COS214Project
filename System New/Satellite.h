a5a031d766d4ab988aa1025e5a63a84fee14b3cc
lisaJFellingham <93034244+lisaJFellingham@users.noreply.github.com>
2021-11-23 08:52:46 +0200

    Update Satellite.h

--- System New/Satellite.h
+++ System New/Satellite.h
@@ -19,17 +19,79 @@ private:
     string name;
 
 public:
-    Satellite(GroundReceiver* gr,string n);             //mediator dp
-    virtual void MessageChanged();                      //mediator dp
-    virtual string getMessage();                        //mediator dp
-    virtual void setMessage(string msg);                //mediator dp
+     /**
+        @brief constructor for Mediator dp
+        @param GroundReceiver
+        @return none, it is void 
+    */
+    Satellite(GroundReceiver* gr,string n);        
+    
+     /**
+        @brief virtual function that notifies others when msg is changed for mediator dp
+        @param none
+        @return none, it is void 
+    */
+    virtual void MessageChanged();                 
+    
+     /**
+        @brief virtual function that gets the message sent out for mediator dp
+        @param none
+        @return string 
+    */
+    virtual string getMessage();                    
+    
+     /**
+        @brief sets the messae for mediator dp
+        @param string
+        @return none, it is void 
+    */
+    virtual void setMessage(string msg);         
+    
+    
 
+     /**
+        @brief Satellite constructor for prototype dp
+        @param string
+        @return none, it is void 
+    */
     Satellite(string name);
+    
+     /**
+        @brief pure virtual launch
+        @param none
+        @return none, it is void 
+    */
     virtual void launch()=0;
+    
+    
     //  void communicate(Satellite* ab);
+    
+     /**
+        @brief pure virtual setName
+        @param string
+        @return none, it is void 
+    */
     virtual void setName(string n) = 0;
+    
+     /**
+        @brief pure virtual getName
+        @param none
+        @return string
+    */
     virtual string getName() = 0;
+    
+     /**
+        @brief pure virtual land
+        @param none
+        @return none, it is void 
+    */
     virtual void land() =0;
+    
+     /**
+        @brief pure virtual getType
+        @param none
+        @return string
+    */
     virtual string getType() = 0;
 
 
