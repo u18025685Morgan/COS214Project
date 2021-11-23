9f2c7b432bee8da58f4f958f24d3dacbbe2d6e6f
lisaJFellingham <93034244+lisaJFellingham@users.noreply.github.com>
2021-11-23 08:14:12 +0200

    Update Command.h

--- System New/Command.h
+++ System New/Command.h
@@ -10,17 +10,17 @@
 
 class Command {
 private:
-    Rocket* RocketReceiver;
-    Engine* EngineReceiver;
+    Rocket* RocketReceiver; //receiver attribute of type Rocket 
+    Engine* EngineReceiver; //receiver attribute of type Engine
 
 public:
-    Command(Rocket* r);
-    Command(Engine* e);
-    virtual void execute()=0;
-    virtual void undo()=0;
+    Command(Rocket* r); //constructor that takes Rocket type if needing to deal with a rocket
+    Command(Engine* e); //constructor that takes Engine type if needing to deal with a engine
+    virtual void execute()=0; //pure virtual execute 
+    virtual void undo()=0;      //pure virtual undo 
 
-    Rocket* getRocketReceiver();
-    Engine* getEngineReceiver();
+    Rocket* getRocketReceiver();    //returns the receiver of type Rocket
+    Engine* getEngineReceiver();//returns the receiver of type Engine
 };
 
 
