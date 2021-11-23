d1221c625354b333eded9c795ed5e2b9c9fc179e
lisaJFellingham <93034244+lisaJFellingham@users.noreply.github.com>
2021-11-23 08:35:18 +0200

    Update TurnOff.h

--- System New/TurnOff.h
+++ System New/TurnOff.h
@@ -9,9 +9,33 @@
 
 class TurnOff : public Command{
 public:
+    /**
+        @brief constructor that initialises RocketReceiver from Command class  
+        @param Rocket 
+        @return none, it is void 
+    */
     TurnOff(Rocket* r);
+    
+    /**
+        @brief constructor that initialises EngineReceiver from Command class  
+        @param Engine 
+        @return none, it is void 
+    */
     TurnOff(Engine* e);
+    
+     /**
+        @brief calls the Rocket or Engine Receiver's off() function
+        @param none
+        @return none, it is void 
+    */
     void execute();
+    
+    
+    /**
+        @brief calls the Rocket or Engine Receiver's on() function
+        @param none
+        @return none, it is void 
+    */
     void undo();
 };
 
