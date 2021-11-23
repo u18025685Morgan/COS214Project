47b37ee78503f796df5901927fa04be7039c134b
lisaJFellingham <93034244+lisaJFellingham@users.noreply.github.com>
2021-11-23 08:16:01 +0200

    Update Command.cpp

--- System New/Command.cpp
+++ System New/Command.cpp
@@ -4,24 +4,24 @@
 
 #include "Command.h"
 
-Command ::Command(Rocket* r)
+Command ::Command(Rocket* r) //constructor that initialises RocketReceiver with @param r
 {
     RocketReceiver = r;
     EngineReceiver = nullptr;
 }
 
-Command ::Command(Engine* e)
+Command ::Command(Engine* e) //constructor that initialises RocketReceiver with @ param e
 {
     EngineReceiver = e;
     RocketReceiver = nullptr;
 }
 
-Rocket* Command ::getRocketReceiver()
+Rocket* Command ::getRocketReceiver() // @return Rocket object 
 {
     return RocketReceiver;
 }
 
-Engine* Command ::getEngineReceiver()
+Engine* Command ::getEngineReceiver() //@return Engine object 
 {
     return EngineReceiver;
 }
