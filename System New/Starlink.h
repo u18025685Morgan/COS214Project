fb7fd80bb80c021d1d8d3cd43c33e5a4618abf65
lisaJFellingham <93034244+lisaJFellingham@users.noreply.github.com>
2021-11-23 08:47:42 +0200

    Update Starlink.h

--- System New/Starlink.h
+++ System New/Starlink.h
@@ -18,17 +18,75 @@ private:
 
 
 public:
-    Starlink(GroundReceiver* gr,string n);      //mediator dp
-    string getMessage();                        //mediator dp
-    void setMessage(string msg);                //mediator dp
-    void MessageChanged();                      //mediator dp
-
-
+    /**
+        @brief Constructor for Starlink for Mediator dp
+        @param GroundReciever object and string
+        @return none, it is void 
+    */
+    Starlink(GroundReceiver* gr,string n);      
+    
+    /**
+        @brief returns message sent out for Mediator dp
+        @param none
+        @return string 
+    */
+    string getMessage();                       
+    
+    /**
+        @brief sets message that needs to be sent out, calls MessageChanged() function for Mediator dp
+        @param string
+        @return none, it is void 
+    */
+    void setMessage(string msg);     
+    
+    /**
+        @brief notifies other satellites about the msg change
+        @param none
+        @return none, it is void 
+    */
+    void MessageChanged();                   
+    
+    
+    /**
+        @brief Constructor for Prototype dp
+        @param string
+        @return none, it is void 
+    */
     Starlink(string n);
+    
+    /**
+        @brief launches satelite
+        @param none
+        @return none, it is void 
+    */
     void launch();
+    
+    /**
+        @brief gets Satellite name
+        @param none
+        @return string 
+    */
     string getName();
+    
+    /**
+        @brief set's Satellite name
+        @param string
+        @return none, it is void 
+    */
     void setName(string n);
+    
+     /**
+        @brief  gets type
+        @param none
+        @return string 
+    */
     string getType();
+    
+     /**
+        @brief lands Satellite
+        @param none
+        @return none, it is void 
+    */
     void land();
 
 
